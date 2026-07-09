13dd2ac2b798e7300cfdc9794607eccee3d48974
salendra rakha <salendraraka7@gmail.com>
2026-07-09 18:42:42 +0700

    Refactor router imports and configuration
    Removed duplicate imports and cleaned up router configuration.

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,7 +2,47 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import MainLayout from "../components/MainLayout";
 import DetailKelas from "../pages/DetailKelas";
+import ProfileGuru from "../pages/ProfileGuru";import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import MainLayout from "../components/MainLayout";
+import DetailKelas from "../pages/DetailKelas";
 import ProfileGuru from "../pages/ProfileGuru";
+import TentangKami from "../pages/TentangKami";
+import DaftarSekarang from "../components/DaftarSekarang";
+import HubungiKami from "../pages/HubungiKami";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "tentang-kami",
+        element: <TentangKami />,
+      },
+      {
+        path: "detail-kelas",
+        element: <DetailKelas />,
+      },
+      {
+        path: "profile-guru",
+        element: <ProfileGuru />,
+      },
+      {
+        path: "daftar-sekarang",
+        element: <DaftarSekarang />,
+      },
+      {
+        path: "hubungi-kami",
+        element: <HubungiKami />,
+      },
+    ],
+  },
+]);
 import TentangKami from "../pages/TentangKami";
 import DaftarSekarang from "../components/DaftarSekarang";
 
